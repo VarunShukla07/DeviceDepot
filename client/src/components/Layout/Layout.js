2e1a4e3220a6ccf097f7dee1c03fcb084c60435c
VarunShukla07 <varun.shuklaoffical@gmail.com>
2024-07-05 15:28:06 +0530

    Register and Login Done

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -3,6 +3,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 import { Helmet } from "react-helmet";
 import { Toaster } from "react-hot-toast";
+
 const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
