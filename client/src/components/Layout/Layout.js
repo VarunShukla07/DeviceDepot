41b4537eee80aa5302a71fce29cb5d80ea41fc68
VarunShukla07 <varun.shuklaoffical@gmail.com>
2024-07-05 12:50:24 +0530

    SEO optimized

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,15 +1,33 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-
-const Layout = ({ children }) => {
+import { Helmet } from "react-helmet";
+import { Toaster } from "react-hot-toast";
+const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
+      </Helmet>
       <Header />
-      <main style={{ minHeight: "80vh" }}>{children}</main>
+      <main style={{ minHeight: "78vh" }}>
+        <Toaster />
+        {children}
+      </main>
       <Footer />
     </div>
   );
+};
+
+Layout.defaultProps = {
+  title: "DeviceDepot - shop now",
+  description: "mern stack project",
+  keywords: "mern,react,node,mongodb",
+  author: "VarunShukla",
 };
 
 export default Layout;
