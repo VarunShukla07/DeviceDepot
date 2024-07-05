7df3eddfb0b8e92ad4d985304e7be62ab7e0109c
VarunShukla07 <varun.shuklaoffical@gmail.com>
2024-07-05 23:08:39 +0530

    Category Price pagination filters

--- client/src/pages/Admin/Products.js
+++ client/src/pages/Admin/Products.js
@@ -24,36 +24,36 @@ const Products = () => {
   }, []);
   return (
     <Layout>
+  <div className="row">
+  <div className="col-md-3 mt-8 pt-3">
+  <AdminMenu />
+</div>
+    <div className="col-md-9">
+      <h1 className="text-center">All Products List</h1>
       <div className="row">
-        <div className="col-md-3">
-          <AdminMenu />
-        </div>
-        <div className="col-md-9 ">
-          <h1 className="text-center">All Products List</h1>
-          <div className="d-flex">
-            {products?.map((p) => (
-              <Link
-                key={p._id}
-                to={`/dashboard/admin/product/${p.slug}`}
-                className="product-link"
-              >
-                <div className="card m-2" style={{ width: "18rem" }}>
-                  <img
-                    src={`/api/v1/product/product-photo/${p._id}`}
-                    className="card-img-top"
-                    alt={p.name}
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">{p.name}</h5>
-                    <p className="card-text">{p.description}</p>
-                  </div>
+        {products?.map((p) => (
+          <div key={p._id} className="col-md-4 mb-4">
+            <Link to={`/dashboard/admin/product/${p.slug}`} className="product-link">
+              <div className="card" style={{ width: "18rem" }}>
+                <img
+                  src={`/api/v1/product/product-photo/${p._id}`}
+                  className="card-img-top"
+                  alt={p.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{p.name}</h5>
+                  <p className="card-text">{p.description}</p>
                 </div>
-              </Link>
-            ))}
+              </div>
+            </Link>
           </div>
-        </div>
+        ))}
       </div>
-    </Layout>
+    </div>
+  </div>
+</Layout>
+
+
   );
 };
 
